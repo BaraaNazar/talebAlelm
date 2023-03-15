870a5a1aa2970cb521af7f26eb2d2960fda74b84
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-03-15 13:25:02 +0300

    completed issue

---
+++ src/containers/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Index from '../components/signUp/index.jsx'
+function SignUp() {
+  return (
+    <div>
+        <Index/>
+    </div>
+  )
+}
+
+
+export default SignUp
