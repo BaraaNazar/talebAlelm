89227b7acf3437cc79dbefefdc503e159a29553e
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-03-23 11:36:46 +0300

    Completed issue

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["kitabat.blog", "www.gph.gov.sa", "clcdn.spa.gov.sa", "www.wmn.gov.sa", "2.bp.blogspot.com", "alkafeel.net", "www.ahmadiyya-islam.org", "upload.wikimedia.org", "media.licdn.com"],
+    domains: [
+      "kitabat.blog",
+      "www.ahmadiyya-islam.org",
+      "clcdn.spa.gov.sa",
+      "www.wmn.gov.sa",
+      "2.bp.blogspot.com",
+      "alkafeel.net",
+      "upload.wikimedia.org",
+      "media.licdn.com",
+    ],
   },
   experimental: {
     appDir: true,
   },
-  distDir: 'build',
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
