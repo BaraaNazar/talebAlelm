9560baa78e91a97be8d2e74f49a174eab2471ba6
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-06-24 12:28:22 +0300

    making database in firebase

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyB2fCcbxd6m5LG43c51psnkfIcezySUr-o',
@@ -14,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const database = getFirestore(app);
